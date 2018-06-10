edfee127b79142b1ead151b3b59902ec1a431158
Dan Coventry <daniel.coventry2@gmail.com>
2018-06-10 18:29:00 +0100

    adds tests and full implementation of a linear search

--- src/search/LinearSearch.java
+++ src/search/LinearSearch.java
@@ -1,7 +1,23 @@
 package search;
 
+/**
+ * class containing a method which performs a linear search on an array of integers.
+ */
 public class LinearSearch {
+
+    /**
+     * Searches an array of integers linearly to try and find the target integer.
+     * @param target The integer being searched for.
+     * @param values An Array of integers that will be searched for the target, this  does not have to be ordered.
+     * @return Boolean for if values contains the target.
+     */
     public boolean search(int target, int[] values){
-        return true;
+        for(int i : values) {
+            if (i == target) {
+                return true;
+            }
+        }
+        return false;
     }
+
 }
--- test/search/LinearSearchTest.java
+++ test/search/LinearSearchTest.java
@@ -17,9 +17,33 @@ public class LinearSearchTest {
         assertTrue(searcher.search(target, values));
     }
 
+    //tests that it compares values correctly when comparing with a different value.
     @Test
     public void comparisonWithDifferentValue(){
+        LinearSearch searcher = new LinearSearch();
+        int target = 5;
+        int[] values = new int[1];
+        values[0] = 2;
 
+        assertFalse(searcher.search(target, values));
+    }
+
+    //tests that it moves to the next value in array if current value != target
+    @Test
+    public void movesToNextValue(){
+        LinearSearch searcher = new LinearSearch();
+        int target = 2;
+        int[] values = {1, 2};
+        assertTrue(searcher.search(target, values));
+    }
+
+    //tests that it returns false when target is not in values array
+    @Test
+    public void targetNotInValues(){
+        LinearSearch searcher = new LinearSearch();
+        int target = 5;
+        int[] values = {1, 3, 4, 2};
+        assertFalse(searcher.search(target, values));
     }
 
 }
